69bc9f26a245ee7ee4566d99f0b30a579c7220b3
Victor Pakhomov <1146089+fossil@users.noreply.github.com>
2018-08-23 21:15:47 +0200

    added Task entity

---
+++ src/main/java/org/pakhomov/tasks/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package org.pakhomov.tasks.repository;
+
+import org.pakhomov.tasks.domain.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data MongoDB repository for the Task entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+}
